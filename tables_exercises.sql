b602cf779127c46a9ab66e6bee3ee3cf4ddf80ee
Jacob Panyathong <jacobpanyathong1@gmail.com>
2022-10-24 17:01:22 -0400

    tables_exercises.sql

---
+++ tables_exercises.sql
@@ -0,0 +1,20 @@
+SHOW DATABASES;
+
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE dept_manager;
+
+SHOW CREATE TABLE dept_manager;
+
+
+
+/* 5. I pulled the following data types: 
+	int, date, VARCHAR, CHAR, boolean no null, and primary key /*
+-- 6. employees and others
+-- 7. eployeees and others
+-- 8. eployees and others will have date type columns
+/* 9. both tables have no NULL, 
+	both tables have a primary key. 
+	(there is no apparent common relationship between the two tables.)
